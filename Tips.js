d4181a6468500f47081f51e74642a1bba249bd3c
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2025-03-28 16:02:15 -0700

    Initial Commit | 28 Mar 25 | Tips
    Data for the Tips Generator

---
+++ Tips.js
@@ -0,0 +1,7 @@
+export const breastfeedingTips = [
+    "Stay hydrated and drink water after every feeding or pumping session.",
+    "Try to maintain a balanced diet with plenty of nutrients to support milk production.",
+    "Relax and take deep breaths while nursing or pumping to improve let-down reflex.",
+    "Skin-to-skin contact with your baby can help increase milk supply.",
+    "Pump or nurse frequently to stimulate production, especially during growth spurts.",
+]
