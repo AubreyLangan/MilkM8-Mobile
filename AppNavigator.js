10fe81d53c258e9b8c31267d83e637924abbfa60
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2025-04-04 09:21:11 -0700

    Commit | 4 Apr 25 | App Navigator
    Changes made to improve the component; additional code added

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,18 +1,45 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+
+
 import HomeScreen from "../screens/HomeScreen";
 import ProfileScreen from "../screens/ProfileScreen";
+import ProgressTrendsScreen from "../screens/ProgressTrendsScreen";
+import AboutScreen from "../screens/AboutScreen";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-const AppNavigator = () => (
-    <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Profile" component={ProfileScreen} />
+const TabNavigator = () => {
+    return (
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Profile" component={ProfileScreen} />
+            <Tab.Screen name="Progress" component={ProgressTrendsScreen} />
+            <Tab.Screen name="Trackers" component={FeedTrackerScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+    );
+};
+
+const StackNavigator = () => {
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Main" component={TabNavigator} />
+            <Stack.Screen name="About" component={AboutScreen} />
+            <Stack.Screen name="SignIn" component={SignInScreen} />
+            <Stack.Screen name="SignUp" component={SignUpScreen} />
         </Stack.Navigator>
-    </NavigationContainer>
-);
+    );
+};
+
+const AppNavigator = () => {
+    return (
+        <NavigationContainer>
+            <StackNavigator />
+        </NavigationContainer>
+    );
+};
 
 export default AppNavigator;
