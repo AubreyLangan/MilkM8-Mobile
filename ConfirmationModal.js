b53e37475352e15f3223ff0f24a14dbcf4c24ec6
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2025-04-26 10:34:16 -0700

    Commit 2 | 26 Apr 25 | Confirmation Modal
    Stylings added

--- ConfirmationModal.js
+++ ConfirmationModal.js
@@ -8,7 +8,7 @@ const ConfirmationModal = ({ isOpen, message, onConfirm, onCancel }) => {
             transparent
             animationType="fade"
         >
-            <View style={styles.overlap}>
+            <View style={styles.overlay}>
                 <View style={styles.modal}>
                     <Text style={styles.message}>{message}</Text>
                     <View style={styles.buttonContainer}>
@@ -25,3 +25,46 @@ const ConfirmationModal = ({ isOpen, message, onConfirm, onCancel }) => {
     );
 };
 
+const styles = StyleSheet.create({
+    overlay: {
+        flex: 1,
+        backgroundColor: "rgba(0,0,0,0.5)",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    modal: {
+        width: "80%",
+        backgroundColor: "#fff",
+        borderRadius: 10,
+        padding: 20,
+        alignItems: "center",
+    },
+    message: {
+        fontSize: 16,
+        marginBottom: 20,
+        textAlign: "center",
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        gap: 10,
+    },
+    confirmButton: {
+        backgroundColor: "#0288d1",
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+        marginRight: 10,
+    },
+    cancelButton: {
+        backgroundColor: "#ccc",
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: "#fff",
+        fontWeight: "bold",
+    },
+});
+
+export default ConfirmationModal;
